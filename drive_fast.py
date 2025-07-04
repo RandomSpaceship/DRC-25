c073e862e417aeba284bbc1e06fa022f23d8156a
James N <59348282+RandomSpaceship@users.noreply.github.com>
2025-07-04 17:34:44 +1000

    post comp

--- drive_fast.py
+++ drive_fast.py
@@ -8,9 +8,9 @@ drive_out = hardware.MecanumHardwareAPI(
 )
 drive_out.open()
 
-for i in range(0, 50):
+while True:
     # drive_out.write_speeds(400, 400)
-    drive_out.update(-300, 0, 0)
+    drive_out.update(600, -600, 600)
     time.sleep(0.05)
 
 drive_out.close()
